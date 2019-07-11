9e2c9d1bfaeaa08e629d0fde2db258c9696833d5
Jarod Raedels <jarod.raedels@publicissapient.com>
2019-07-11 13:14:59 -0400

    assistant expansion panel with typing, need to fix keyboard_arrow_down

--- src/components/Assistant.vue
+++ src/components/Assistant.vue
@@ -1,59 +1,17 @@
 <template>
-  <div class="assistant">
-  <v-layout
-    wrap
-    style="height: 200px;"
-  >
-    <v-container>
-      <v-layout justify-center>
-        <v-btn
-          color="pink"
-          dark
-          @click.stop="drawer = !drawer"
-        >
-          Toggle
-        </v-btn>
-      </v-layout>
-    </v-container>
-
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-      right
-      persistent
+  <v-expansion-panel>
+    <v-expansion-panel-content
     >
-      <v-list class="pa-1">
-        <v-list-tile avatar>
-          <v-list-tile-avatar>
-            <img src="https://randomuser.me/api/portraits/men/85.jpg">
-          </v-list-tile-avatar>
-
-          <v-list-tile-content>
-            <v-list-tile-title>John Leider</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-
-      <v-list class="pt-0" dense>
-        <v-divider></v-divider>
-
-        <v-list-tile
-          v-for="item in items"
-          :key="item.title"
-        >
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-  </v-layout>
-  </div>
+      <template v-slot:header>
+        <div>Plan better here</div>
+      </template>
+      <v-card>
+        <v-card-text>{{ response }}</v-card-text>
+        <v-text-field label="Input" v-model="input"></v-text-field>
+        <v-btn color="blue darken-1" flat @click="sendInput">Respond</v-btn>
+      </v-card>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
 </template>
 
 <script>
@@ -61,19 +19,21 @@ export default {
   name: 'Assistant',
   data () {
     return {
-      drawer: null,
-      items: [
-        { title: 'Home', icon: 'dashboard' },
-        { title: 'About', icon: 'question_answer' }
-      ]
+      input: ''
+    }
+  },
+  methods: {
+    sendInput () {
+      this.input = ''
+    }
+  },
+  computed: {
+    response: function () {
+      return 'How can I help you?'
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
-.pa-1 {
-    display: right;
-}
+<style>
 </style>
