d59fcc818104760c3a975661113866a7ad7d3fbd
Richa Virmani <42806102+rvirmani6@users.noreply.github.com>
2019-07-12 11:53:01 -0400

    VI

--- src/components/Assistant.vue
+++ src/components/Assistant.vue
@@ -1,17 +1,59 @@
+// eslint-disable-next-line
+/* eslint-disable */
 <template>
-  <v-expansion-panel>
-    <v-expansion-panel-content
+  <div class="assistant">
+  <v-layout
+    wrap
+    style="height: 100%;"
+  > <!-- make vertical along right side -->
+    <v-container>
+      <v-layout justify-center>
+        <v-btn
+          color="pink"
+          dark
+          @click.stop="drawer = !drawer"
+        >
+          Toggle
+        </v-btn>
+      </v-layout>
+    </v-container>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
     >
-      <template v-slot:header>
-        <div>Plan better here</div>
-      </template>
-      <v-card>
-        <v-card-text>{{ response }}</v-card-text>
-        <v-text-field label="Input" v-model="input"></v-text-field>
-        <v-btn color="blue darken-1" flat @click="sendInput">Respond</v-btn>
-      </v-card>
-    </v-expansion-panel-content>
-  </v-expansion-panel>
+      <v-list class="pa-1">
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img src="https://randomuser.me/api/portraits/men/85.jpg">
+          </v-list-tile-avatar>
+
+          <v-list-tile-content>
+            <v-list-tile-title>John Leider</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+
+        <v-list-tile
+          v-for="item in items"
+          :key="item.title"
+          v-on@click="">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </v-layout>
+  </div>
 </template>
 
 <script>
@@ -19,21 +61,19 @@ export default {
   name: 'Assistant',
   data () {
     return {
-      input: ''
-    }
-  },
-  methods: {
-    sendInput () {
-      this.input = ''
-    }
-  },
-  computed: {
-    response: function () {
-      return 'How can I help you?'
+      drawer: null,
+      items: [
+        { title: 'Home', icon: 'dashboard' },
+        { title: 'About', icon: 'question_answer' }
+      ]
     }
   }
 }
 </script>
 
-<style>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.pa-1 {
+    display: right;
+}
 </style>
