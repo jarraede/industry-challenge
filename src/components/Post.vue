63df28fdd2bd37b436a61ec6e4d225938b02b62d
Richa Virmani <42806102+rvirmani6@users.noreply.github.com>
2019-07-12 11:11:34 -0400

    image sizes corrected :)

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -5,7 +5,7 @@
                 <v-flex xs12 sm6 md4 lg3 v-for="place in list" :key="place.name">
                     <v-card flat class = "text-xs-center ma-3">
                         <v-responsive class = "pt-4">
-                            <img :src= place.image>
+                            <img :src= place.image style="width:400px;height:300px;">
                         </v-responsive>
                         <v-card-text>
                             <div class="heading"> {{""}} </div>
@@ -26,21 +26,21 @@
 <script>
 export default {
   name: 'Post',
-  data() {
-      return {
-        list: [
-            {name: 'New York', image: "https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200"},
-            {name: 'Hawaii', image: "https://www.fodors.com/wp-content/uploads/2018/12/UltimateHawaii__HERO_shutterstock_352827422.jpg"},
-            {name: 'Rome', image: "https://lonelyplanetimages.imgix.net/mastheads/stock-photo-roman-sunset-77415821.jpg?sharp=10&vib=20&w=1200"},
-            {name: 'Cancun', image: "http://services.travelsavers.com/AMGService.svc/REST/GetImage?ImageID=aa5edd46-7636-e811-853f-782bcb667b27&Width=450"},
-            {name: 'Orlando', image: "https://cdn.thecrazytourist.com/wp-content/uploads/2018/08/ccimage-shutterstock_674895937.jpg"},
-            {name: 'London', image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall.jpg/1000px-Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall.jpg"},
-            {name: 'Miami', image: "https://img1.coastalliving.timeinc.net/sites/default/files/styles/landscape_3_2/public/image/2017/01/main/miami-florida-luxury-destination-2017-144863422.jpg"},
-            {name: 'Las Vegas', image: "https://s-ec.bstatic.com/images/hotel/max1024x768/170/170504164.jpg"},
-            {name: 'Tahiti', image: "https://www.tahitilegends.com/images/territories/tahiti.jpg"},
-            {name: 'Bora Bora', image: "https://s-ec.bstatic.com/images/hotel/max1024x768/149/149150580.jpg"}
-        ]
-      }
+  data () {
+    return {
+      list: [
+        {name: 'New York', image: 'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200'},
+        {name: 'Hawaii', image: 'https://www.fodors.com/wp-content/uploads/2018/12/UltimateHawaii__HERO_shutterstock_352827422.jpg'},
+        {name: 'Rome', image: 'https://lonelyplanetimages.imgix.net/mastheads/stock-photo-roman-sunset-77415821.jpg?sharp=10&vib=20&w=1200'},
+        {name: 'Cancun', image: 'http://services.travelsavers.com/AMGService.svc/REST/GetImage?ImageID=aa5edd46-7636-e811-853f-782bcb667b27&Width=450'},
+        {name: 'Orlando', image: 'https://cdn.thecrazytourist.com/wp-content/uploads/2018/08/ccimage-shutterstock_674895937.jpg'},
+        {name: 'London', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall.jpg/1000px-Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall.jpg'},
+        {name: 'Miami', image: 'https://img1.coastalliving.timeinc.net/sites/default/files/styles/landscape_3_2/public/image/2017/01/main/miami-florida-luxury-destination-2017-144863422.jpg'},
+        {name: 'Las Vegas', image: 'https://s-ec.bstatic.com/images/hotel/max1024x768/170/170504164.jpg'},
+        {name: 'Tahiti', image: 'https://www.tahitilegends.com/images/territories/tahiti.jpg'},
+        {name: 'Bora Bora', image: 'https://s-ec.bstatic.com/images/hotel/max1024x768/149/149150580.jpg'}
+      ]
+    }
   }
 }
 </script>
